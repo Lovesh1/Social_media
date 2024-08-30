6526e4c3122ddbe84fee921ef2ffc852d9cf7d93
Lovesh1 <lovesh688@gmail.com>
2024-08-31 04:14:02 +0530

    Initial: customUser and django setup

---
+++ socialmedia/users/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .manager import CustomUserManager
+from django.core.validators import RegexValidator
+from django.utils import timezone
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,10}$', message="Phone number must be entered in the format: '+999999999'. Up to 10 digits allowed")
+    phone_number = models.CharField(max_length=10, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['email']
+    
+    def __str__(self):
+        return self.phone_number
